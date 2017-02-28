4d3dae4fd7813362ab066a6c698bb57eb554a275
wondercee <cecilia.barbecot@gmail.com>
2017-02-28 12:20:46 +0000

    Add files via upload

---
+++ autotweet.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import tweepy, time, sys
+
+argfile = str(sys.argv[1])
+
+#enter the corresponding information from your Twitter application:
+CONSUMER_KEY = 'lu4iEecPQFZn4s9LGRPpYGUWb'
+CONSUMER_SECRET = 'oFg9st50Yjd7ULxrYyuhegupLYbY0N90Dfajd6WOZfWW0Njdj0'
+ACCESS_KEY = '834031117237907456-mCThq0J1KduyGb6yAQNytOCkBJDajJo'
+ACCESS_SECRET = 'ZxwDhlAwqKNdEQvaLqYMfdnHaEBvjBhRjAnFayOKu0rmi'
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+filename=open(argfile,'r')
+f=filename.readlines()
+filename.close()
+
+for line in f:
+    api.update_status(line)
+    time.sleep(900)#Tweet every 15 minutes
---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+	consumer_key =          'QVPArUrwRNCnkdq462lBByXLF',
+	consumer_secret =       'QDz6QBaRlY4asa9Rg987KbZQQYwN4SHT3Uo8553HXGzOd7rRiF',
+	access_token =          '834031117237907456-PVzDuOS77deIoFLNdMXR9E8gg0iDm2l',
+	access_token_secret =   'jqE7oRHfv5GCf1q2ItqVghxXo5KQ6mVgOWgskzUHsqZGt',
+)
